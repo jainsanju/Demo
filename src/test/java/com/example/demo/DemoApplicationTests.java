791a9af677d1e80b783bf9661a44f01e190a81d2
jainsanju <jainsjain92098@gmail.com>
2024-12-06 14:18:47 +0530

    Testcase fix

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class DemoApplicationTests {
+@TestPropertySource(properties = "server.servlet.context-path=/demo")
+class YourApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext);
+    }
 }
